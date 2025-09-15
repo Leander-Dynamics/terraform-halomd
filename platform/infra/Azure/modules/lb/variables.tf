36b8debfd7995af48b2ad77d6ae0e3fdae327b02
georgejpark <parkgeo@gmail.com>
2025-09-15 18:52:54 -0500

    Define lb module variable types

--- platform/infra/Azure/modules/lb/variables.tf
+++ platform/infra/Azure/modules/lb/variables.tf
@@ -1,5 +1,25 @@
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "sku" { default = "Standard" }
-variable "public_ip_id" {}
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "sku" {
+  type    = string
+  default = "Standard"
+
+  validation {
+    condition     = contains(["Standard", "Basic"], var.sku)
+    error_message = "The sku must be either \"Standard\" or \"Basic\"."
+  }
+}
+
+variable "public_ip_id" {
+  type = string
+}
