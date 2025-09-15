adf8a65b884a5d808deee76a95d22440b110d69e
georgejpark <parkgeo@gmail.com>
2025-09-15 17:25:28 -0500

    feat: require NIC id for linux VM module

--- platform/infra/Azure/modules/linux-virtual-machine/variables.tf
+++ platform/infra/Azure/modules/linux-virtual-machine/variables.tf
@@ -3,5 +3,11 @@ variable "location" {}
 variable "resource_group_name" {}
 variable "sku" { default = "Standard" }
 variable "nic_id" {
-  default = ""
+  description = "Resource ID of the primary network interface to attach to the VM."
+  type        = string
+
+  validation {
+    condition     = can(regex("^/subscriptions/[^/]+/resourceGroups/[^/]+/providers/Microsoft\\.Network/networkInterfaces/[^/]+$", trimspace(var.nic_id)))
+    error_message = "nic_id must be a valid Azure resource ID for a network interface."
+  }
 }
