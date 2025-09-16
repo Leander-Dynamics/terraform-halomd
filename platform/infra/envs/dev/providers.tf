d167bd67c446fd8c8788415c2eff0e806c793da2
georgejpark <parkgeo@gmail.com>
2025-09-16 08:38:59 -0500

    Remove duplicate required providers block

--- platform/infra/envs/dev/providers.tf
+++ platform/infra/envs/dev/providers.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.33.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
 locals {
   provider_subscription_id = try(trimspace(var.subscription_id), "") != "" ? var.subscription_id : null
   provider_tenant_id       = try(trimspace(var.tenant_id), "") != "" ? var.tenant_id : null
