48ee677d91d012c0ae8d091b5fb99ac7f144779d
georgejpark <parkgeo@gmail.com>
2025-09-15 21:31:31 -0500

    Pin azuread and random provider versions

--- platform/infra/envs/dev/versions.tf
+++ platform/infra/envs/dev/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.0"
   required_providers {
     azurerm = { source = "hashicorp/azurerm", version = "~>4.33" }
-    azuread = { source = "hashicorp/azuread", version = "<latest>" }
-    random  = { source = "hashicorp/random",  version = "<latest>" }
+    azuread = { source = "hashicorp/azuread", version = "~>2.40" }
+    random  = { source = "hashicorp/random",  version = "~>3.5" }
   }
 }
