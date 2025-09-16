2e2f04f82c868f986abad3cb413a66ae988714d9
georgejpark <parkgeo@gmail.com>
2025-09-15 20:04:06 -0500

    Add tags support to managed disk module

--- platform/infra/Azure/modules/managed-disk/main.tf
+++ platform/infra/Azure/modules/managed-disk/main.tf
@@ -5,4 +5,5 @@ resource "azurerm_managed_disk" "this" {
   storage_account_type = var.storage_account_type
   disk_size_gb         = var.disk_size_gb
   create_option        = "Empty"
+  tags                 = var.tags
 }
